79350f00c7cba5e74d53e480d5923b49de1e47ca
puja809 <pujachatterjee809@gmail.com>
2025-12-06 23:34:05 +0530

    committing java code for FOR LOOP Calender problem!!

---
+++ Office_calender_simple.java
@@ -0,0 +1,11 @@
+public class Office_calender_simple {
+    public static void  main(String[] args){
+        for(int i=1;i<=5;i++){
+            System.out.println("Day-"+i);
+            for(int j=1;j<=9;j++){
+                System.out.println((j+8)+"-"+(j+9));
+            }
+
+        }
+    }
+}
