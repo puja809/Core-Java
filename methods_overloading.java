9cefe661c89b99953e7e9102b821cb8374c4d3a6
puja809 <pujachatterjee809@gmail.com>
2025-12-07 20:06:18 +0530

    committing java code for method overloading practical problem

---
+++ methods_overloading.java
@@ -0,0 +1,15 @@
+class addition{
+    public int add(int n1,int n2){
+        return n1+n2;
+    }
+    public int add(int n1,int n2,int n3){
+        return n1+n2+n3;
+    }
+}
+
+public class methods_overloading {
+    public static void main(String[] args){
+        addition a=new addition();
+        System.out.println(a.add(348,987));
+    }
+}
