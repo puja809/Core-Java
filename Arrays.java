d85be7a1e4e5319b68dc97e13d88a26243d7f289
puja809 <pujachatterjee809@gmail.com>
2025-12-08 19:53:32 +0530

    committing java code for array basic

---
+++ Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args){
+        int arr[]= new int[4];
+        int a[]={1,2,3,4,5,6};
+        for(int i=0;i<6;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
