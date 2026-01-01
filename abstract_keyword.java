1378f7fc3bad0a09ce327a8374af6b34edd24cc3
puja809 <pujachatterjee809@gmail.com>
2026-01-01 14:09:46 +0530

    committing java code for abstract keyword!

---
+++ abstract_keyword.java
@@ -0,0 +1,24 @@
+//abstract methods have to be defined inside abstract class
+abstract class Rocket{
+    public abstract void fly();
+    public void show(){
+        System.out.println("Inside a rocket");
+    }
+}
+
+
+class Plane extends Rocket{
+    public void fly(){
+        System.out.println("This object can fly...");
+    }
+
+}
+
+public class abstract_keyword {
+    public static void main(String[] args){
+        //abstract class can have reference variable but not an object
+        Rocket obj =new Plane();
+        obj.fly();
+        obj.show();
+    }
+}
