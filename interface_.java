3d1ca658ad162415df50fe1cf21a09abbb4e6e16
puja809 <pujachatterjee809@gmail.com>
2026-01-07 12:02:53 +0530

    committing java code for interface!

--- interface_.java
+++ interface_.java
@@ -1,5 +1,7 @@
 // An interface is a blueprint that defines what methods a class must implement.
 // It provides method declarations but no implementation.
+//A class can implement multiple interfaces
+//An interface can extend another interface
 
 interface Gems{
     int age=27;//Variables in interface are by default static, final and public
@@ -8,7 +10,15 @@ interface Gems{
     void color();
 }
 
-class Gemstone implements Gems{
+interface Brand{
+    void show();
+}
+
+
+interface Shoes extends Brand{
+    void shoeBrand();
+}
+class Gemstone implements Gems,Shoes{
 
     public void name() {
         System.out.println("Gold");
@@ -18,15 +28,31 @@ class Gemstone implements Gems{
     public void color() {
         System.out.println("Yellow");
     }
+
+    public void show() {
+        System.out.println("This is show in Brand!");
+    }
+
+
+    public void shoeBrand() {
+        System.out.println("Bata is a shoe brand!!");
+    }
 }
 
 
 public class interface_ {
     public static void main(String []arg){
-        Gems obj; //Gemstone obj=new Gemstone();--This can also be done
-        obj=new Gemstone();
+        //Gems obj; //Gemstone obj=new Gemstone();--This can also be done
+        Gemstone obj=new Gemstone();
+//        obj=new Gemstone();
         obj.color();
         obj.name();
+        Brand obj1;
+//        obj1=new Gemstone();
         System.out.println(Gems.age);
+        obj.show();
+        Shoes obj2;
+//        obj2=new Gemstone();
+        obj.shoeBrand();
     }
 }
